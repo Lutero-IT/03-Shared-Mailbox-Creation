924cd5644af8670c34eaf874455df819d9c3a1c4
Lutero-IT <lutero.it@gmail.com>
2026-06-21 12:23:41 +0200

    feat: add helper function for name validation
    Used Regular Expression to check if the provided name for a Shared
    Mailbox Account meets the requirements and follows corporate standards
    and industry best practices.

--- 03-Shared-Mailbox-Creator.ps1
+++ 03-Shared-Mailbox-Creator.ps1
@@ -1,5 +1,44 @@
+# HELPER FUNCTIONS #
+
+function Approve-Name {
+    param (
+        [Parameter(Mandatory=$true)]
+        [ValidateNotNullOrEmpty()]
+        [string]$Name
+    )
+
+    $regex = '^[a-zA-Z-0-9_\-\.]{5,20}$'
+    return ($Name -match $regex)
+}
+
+<#
+    For Approve-Name
+    Write mechanism that will manage empty input for parameter $Name
+    I can use if/else before function to check if $name is null
+    or can I do it inside function?
+    I could bind CommonParameters and use -ErrorAction and try/catch
+    on the Approve-Name function but is it the best way?
+    Or simply check the input before calling a function?
+#>
+
 Write-Host "Provide name for a Shared Mailbox account"
 $name = Read-Host -Prompt "Type name"
+
+try {
+    $isMatch = Approve-Name -Name $name
+    # I need to get the value from Approve-Name
+    # If it is true I pass the name to the Check User
+    # If not I ask again for correct input
+    if ($isMatch) {
+        Write-Host "Name meet requirements"
+    } else {
+        Write-Host "Name doesn't meet requirements"
+        # loop back to the prompt
+    }
+}
+catch {
+    Write-Host "No input passed"
+}
 
 # Check if user exist in database
 try {
